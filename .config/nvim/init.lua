214deb2c4fb759d12a470e79ee66fdfb29e7ebe5
Demitroi <checo1085@hotmail.com>
2025-03-25 23:02:20 -0700

    Add linters draft

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -114,7 +114,7 @@ require('lazy').setup({
         'https://github.com/lewis6991/gitsigns.nvim',
         config = function()
             require('gitsigns').setup()
-            vim.cmd("Gitsigns toggle_current_line_blame")
+            vim.cmd('Gitsigns toggle_current_line_blame')
             -- Git diff
             vim.keymap.set('n', '<Leader>gd', '<Cmd>:Gitsigns preview_hunk<CR>')
             -- Git previous
@@ -174,7 +174,7 @@ require('lazy').setup({
     {
         'stevearc/oil.nvim',
         config = function()
-            require("oil").setup()
+            require('oil').setup()
             vim.keymap.set('n', '-', '<CMD>Oil<CR>')
         end,
         lazy = false,
@@ -195,7 +195,7 @@ require('lazy').setup({
             {
                 'williamboman/mason-lspconfig.nvim',
                 opts = {
-                    ensure_installed = { "cssls", "eslint", "jdtls", "gopls", "html", },
+                    ensure_installed = { 'cssls', 'eslint', 'jdtls', 'gopls', 'html', },
                 }
             },
         },
@@ -231,10 +231,10 @@ require('lazy').setup({
             require'nvim-treesitter.configs'.setup {
                 -- A list of parser names, or "all" (the listed parsers MUST always be installed)
                 ensure_installed = {
-                    "bash", "css", "desktop", "diff", "dockerfile", "git_config", "gitignore", "go",
-                    "gomod", "gosum", "gotmpl", "gowork", "html", "java", "javascript", "jq", "json",
-                    "lua", "make", "markdown", "markdown_inline", "nginx", "php", "php_only", "python",
-                    "sql", "ssh_config", "templ", "tmux", "toml", "typescript", "vue", "xml", "yaml",
+                    'bash', 'css', 'desktop', 'diff', 'dockerfile', 'git_config', 'gitignore', 'go',
+                    'gomod', 'gosum', 'gotmpl', 'gowork', 'html', 'java', 'javascript', 'jq', 'json',
+                    'lua', 'make', 'markdown', 'markdown_inline', 'nginx', 'php', 'php_only', 'python',
+                    'sql', 'ssh_config', 'templ', 'tmux', 'toml', 'typescript', 'vue', 'xml', 'yaml',
                 },
                 highlight = {
                     enable = true,
@@ -268,9 +268,58 @@ require('lazy').setup({
                 default = { 'lsp', 'path', 'snippets', 'buffer', },
             },
             fuzzy = {
-                implementation = "prefer_rust_with_warning",
+                implementation = 'prefer_rust_with_warning',
             },
         },
-        opts_extend = { "sources.default", },
+        opts_extend = { 'sources.default', },
     },
+    -- Linters: work in progress..
+    -- {
+    --     -- mason-nvim-lint
+    --     -- https://github.com/rshkarin/mason-nvim-lint
+    --     'rshkarin/mason-nvim-lint',
+    --     dependencies = {
+    --         {
+    --             -- mason.nvim
+    --             -- https://github.com/williamboman/mason.nvim
+    --             'williamboman/mason.nvim',
+    --             opts = {},
+    --         },
+    --         {
+    --             -- nvim-lint
+    --             -- https://github.com/mfussenegger/nvim-lint
+    --             'mfussenegger/nvim-lint',
+    --             event = { 'BufReadPre', 'BufNewFile', },
+    --             config = function()
+    --                 local lint = require 'lint'
+    --                 lint.linters_by_ft = {
+    --                     go = { 'golangcilint' },
+    --                     javascript = { 'eslint_d' },
+    --                     typescript = { 'eslint_d' },
+    --                     javascriptreact = { 'eslint_d' },
+    --                     typescriptreact = { 'eslint_d' },
+    --                     html = { 'htmlhint' },
+    --                     css = { 'stylelint' },
+    --                 }
+    --                 local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+    --                 vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+    --                     group = lint_augroup,
+    --                     callback = function()
+    --                         -- Only run the linter in buffers that you can modify in order to
+    --                         -- avoid superfluous noise, notably within the handy LSP pop-ups that
+    --                         -- describe the hovered symbol using Markdown.
+    --                         if vim.opt_local.modifiable:get() then
+    --                             lint.try_lint()
+    --                         end
+    --                     end,
+    --                 })
+    --             end,
+    --         },
+    --     },
+    --     config = function ()
+    --         require("mason-nvim-lint").setup({
+    --             ensure_installed = { 'golangci-lint', 'eslint_d', 'htmlhint', 'stylelint' },
+    --         })
+    --     end,
+    -- },
 })
