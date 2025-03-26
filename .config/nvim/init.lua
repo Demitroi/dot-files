0a8ee67433d500a48947cc25f1cfabf5285943ad
Demitroi <checo1085@hotmail.com>
2025-03-25 18:25:02 -0700

    Add completion plugin and snippets

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -245,4 +245,32 @@ require('lazy').setup({
             }
         end,
     },
+    {
+        -- blink.cmp
+        -- https://github.com/Saghen/blink.cmp
+        'saghen/blink.cmp',
+        dependencies = {
+            -- friendly-snippets
+            -- https://github.com/rafamadriz/friendly-snippets
+            'rafamadriz/friendly-snippets',
+        },
+        version = '1.*',
+        opts = {
+            keymap = {
+                preset = 'default'
+            },
+            completion = {
+                documentation = {
+                    auto_show = false
+                }
+            },
+            sources = {
+                default = { 'lsp', 'path', 'snippets', 'buffer', },
+            },
+            fuzzy = {
+                implementation = "prefer_rust_with_warning",
+            },
+        },
+        opts_extend = { "sources.default", },
+    },
 })
