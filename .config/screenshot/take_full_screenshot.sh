95503e9911818b107f2353790b175bdea7525624
Demitroi <checo1085@hotmail.com>
2026-05-08 17:28:59 -0700

    Improve screenshot scripts

--- .config/screenshot/take_full_screenshot.sh
+++ .config/screenshot/take_full_screenshot.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-screenshot_path=~/Pictures/screenshot_$(date +%Y%m%d%H%M%S).png
+current_date=$(date +%Y%m%d%H%M%S)
+screenshot_path=~/Pictures/screenshot_${current_date}.png
+screenshot_edit_path=~/Pictures/screenshot_edit_${current_date}.png
 grim - | tee $screenshot_path | wl-copy
-cat $screenshot_path | swappy -f -
-
+swappy -f $screenshot_path -o $screenshot_edit_path
