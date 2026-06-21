85990f888aeaccc461cc92f2d22558953892cee0
Demitroi <checo1085@hotmail.com>
2026-06-21 12:46:30 -0700

    The take_full_screenshot script will take only the active display

--- .config/screenshot/take_full_screenshot.sh
+++ .config/screenshot/take_full_screenshot.sh
@@ -2,5 +2,6 @@
 
 current_date=$(date +%Y%m%d-%H%M%S)
 screenshot_path=~/Pictures/screenshot_${current_date}.png
-grim - | tee $screenshot_path | wl-copy
+focused_output=$(swaymsg -t get_outputs | jq -r '.[] | select(.focused) | .name')
+grim -o "$focused_output" - | tee "$screenshot_path" | wl-copy
 swappy -f $screenshot_path
